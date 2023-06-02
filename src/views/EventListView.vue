0abf0dca96bde638e252d213a4ee8fe065673523
Öjvind Otterbjörk <ojvind.otterbjork@icloud.com>
2023-06-02 08:57:09 +0200

    Add db.json - Fake Online REST server for teams

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,42 +1,5 @@
 <script setup>
   import EventCard from '../components/EventCard.vue'
-  import { ref } from 'vue'
-
-  const events = ref([
-    {
-      id: 1,
-      category: 'altnernative',
-      title: 'God Save the Queen',
-      description: 'old-school british punk',
-      location: 'Soho, London',
-      date: '2021-07-04',
-      time: '12:00',
-      petsAllowed: true,
-      organizer: 'Three Cord Records',
-    },
-    {
-      id: 1,
-      category: 'black metal',
-      title: 'Troll Metal Fest',
-      description: 'Enjoy the best of the best in troll metal',
-      location: 'Hammerfest, Norway',
-      date: '2021-07-04',
-      time: '12:00',
-      petsAllowed: true,
-      organizer: 'Lucifer Records',
-    },
-    {
-      id: 1,
-      category: 'metal',
-      title: 'Swallow the Sun',
-      description: 'Finish doom metal',
-      location: 'Helsinki, Finland',
-      date: '2021-07-04',
-      time: '12:00',
-      petsAllowed: true,
-      organizer: 'Metal maniacs',
-    }
-  ])
 </script>
 
 <template>
