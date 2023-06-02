b002b4df99073a1bad6a4e53a66ddedebd88b89a
Öjvind Otterbjörk <ojvind.otterbjork@icloud.com>
2023-06-02 10:38:59 +0200

    Add EventService (with Axios inside)

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,5 +1,14 @@
 <script setup>
+  import { ref, onMounted } from 'vue'
+  import EventService from '../services/EventService.js'
   import EventCard from '../components/EventCard.vue'
+
+  const events = ref(null);
+
+  onMounted(async () => {
+    const response = await EventService.getEvents();
+    events.value = response.data;
+  });
 </script>
 
 <template>
